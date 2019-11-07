ceb5048d5c63d2249bfa8358e21594120d6355cc
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-11-07 19:08:03 +0000

    Create types for notebook and notebook input

--- index.js
+++ index.js
@@ -5,17 +5,32 @@ const { buildSchema } = require("graphql"); // takes template literal
 const app = express();
 app.use(express.json());
 
+const notebookList = [
+  { _id: 1, name: "SQL", createdAt: "2019-11-07T18:55:46.746Z" }
+];
+
 // use graphqlHttp to build schema and root queries/mutations
 app.use(
   "/graphql",
   graphqlHttp({
     schema: buildSchema(`
+    type Notebook {
+      _id: ID!
+      name: String!
+      createdAt: String!
+    }
+
+    input NotebookInput {
+      name: String!
+      createdAt: String!
+    }
+
     type RootQuery {
-      notebooks: [String!]!
+      notebookList: [Notebook!]!
     }
 
     type RootMutation {
-      createNotebook(name: String): String
+      createNotebook(notebookInput: NotebookInput): Notebook
     }
 
     schema {
@@ -24,13 +39,22 @@ app.use(
     }
   `),
     rootValue: {
-      notebooks: () => {
+      notebookList: () => {
         // called when someone looks for notebooks
-        return ["SQL", "NodeJS", "MongoDB"];
+        return notebookList;
       },
       createNotebook: args => {
-        const { name } = args;
-        return name;
+        const { name, createdAt } = args.notebookInput;
+
+        const notebook = {
+          _id: Math.random().toString(),
+          name,
+          createdAt
+        };
+
+        notebookList.push(notebook);
+
+        return notebook;
       }
     },
     graphiql: true // provides an interface
