5c60ff289c4e94f809d97026dea76dd5ebbac548
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-11-07 18:24:52 +0000

    Create schema and root queries and mutations

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql"); // takes template literal
 
 const app = express();
 app.use(express.json())
 
-app.get("/", (req, res, next) => {
-  res.send("Hello World!");
-})
+// use graphqlHttp to build schema and root queries/mutations
+app.use("/graphql", graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      notebooks: [String!]!
+    }
+
+    type RootMutation {
+      createNotebook(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {}, // points at resolvers
+  graphiql: true // provides an interface
+}))
 
 app.listen(5000)
